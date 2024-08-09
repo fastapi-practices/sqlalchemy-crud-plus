e66f848e7467beb561d87b367598d3d28fb23787
Wu Clan <jianhengwu0407@gmail.com>
2024-08-09 14:50:05 +0800

    Prepare for 0.0.4 release

--- sqlalchemy_crud_plus/__init__.py
+++ sqlalchemy_crud_plus/__init__.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from .crud import CRUDPlus as CRUDPlus
-from .errors import ModelColumnError as ModelColumnError
 
-__version__ = 'v0.0.3'
+__version__ = 'v0.0.4'
